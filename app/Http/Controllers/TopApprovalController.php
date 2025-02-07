3f635c4f1429c91af666a41b348ef08ad7a51de1
Reynaldo Jr Baja <reynaldobaja2@gmail.com>
2025-02-07 10:50:46 +0800

    OperationApproval

--- app/Http/Controllers/TopApprovalController.php
+++ app/Http/Controllers/TopApprovalController.php
@@ -2,15 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobDraft;
 use Illuminate\Http\Request;
 
 class TopApprovalController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        $job_drafts = JobDraft::whereNot('status', 'completed')
+            ->with(['jobOrder', 'contentWriter', 'graphicDesigner', 'client'])
+            ->get();
 
-    public function show() {}
+        return view('pages.admin.topmanager.list', compact('job_drafts'));
+    }
 
-    public function edit() {}
+    public function show($id)
+    {
+        $job_draft = JobDraft::with('jobOrder', 'contentWriter', 'graphicDesigner', 'client')->find($id);
+        return view('pages.admin.topmanager.show', compact('job_draft'));
+    }
 
-    public function update() {}
+    public function edit($id)
+    {
+        $job_draft = JobDraft::with('jobOrder', 'contentWriter', 'graphicDesigner', 'client')->find($id);
+        return view('pages.admin.topmanager.show', compact('job_draft'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'signature_admin' => 'required',
+        ]);
+
+        $job_draft = JobDraft::findOrFail($id);
+
+        $job_draft->update([
+            'signature_admin' => $request->signature_admin,
+            'admin_signed' => auth()->user()->id,
+            'status' => 'Approved by Top Manager',
+        ]);
+
+        return redirect()->route('topmanager.approve')->with('Status', 'Job Order Approved Successfully');
+    }
 }
