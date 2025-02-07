828dd257bae4becf6639501278230a8f46db6998
Reynaldo Jr Baja <reynaldobaja2@gmail.com>
2025-02-07 09:47:31 +0800

    controller for all

---
+++ app/Http/Controllers/ClientApprovalController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientApprovalController extends Controller
+{
+    public function index() {}
+
+    public function show() {}
+
+    public function edit() {}
+
+    public function update() {}
+}
