e1584c696a6fc4009bb91070aa5773406314b1ed
Yourboiralph <hernandezralphb@gmail.com>
2025-02-26 14:20:30 +0800

    i needed

---
+++ resources/views/pages/admin/requestForm/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.application')
+
+@section('title', 'Job Order')
+@section('header', 'List of Job Orders')
+
+@section('content')
+<script src="https://cdn.tailwindcss.com"></script>
+
+<style>
+    .custom-shadow {
+        box-shadow: 0 4px 6px rgba(0, 0, 0, .3), 0 1px 3px rgba(0, 0, 0, .3);
+    }
+    .custom-hover-shadow:hover {
+        box-shadow: 0 10px 15px rgba(0, 0, 0, 0), 0 4px 6px rgba(0, 0, 0, 0);
+        transition: box-shadow 0.3s ease;
+    }
+    .custom-focus-ring:focus {
+        outline: none;
+        box-shadow: 0 0 0 1px #fa7011;
+        transition: box-shadow 0.3s ease;
+    }
+    .active-tab {
+        border-bottom: 2px solid #fa7011;
+    }
+</style>
+
+<div class="container mx-auto p-4 sm:p-6">
+        
+</div>
+
+@endsection
